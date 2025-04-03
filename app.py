db4a997308e099501eda879e70e73724b56ff9f7
Juan Antonio Álvarez Trinidad <jaatalvarez@gmail.com>
2025-04-02 22:40:18 -0600

    Se agregó el encabezado de la aplicación

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.title("Análisis de Datos de Vehículos Usados")
+st.header("Explora los gráficos interactivos para entender mejor los datos")
+st.subheader("Este proyecto permite analizar el mercado de vehículos usados en Estados Unidos mediante gráficos interactivos.")
+
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
 
-print("Análisis de Vehículos Usados")
-print("Esta aplicación permite explorar datos de anuncios de venta de coches mediante gráficos interactivos.")
-
-
 if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
-    # crear un histograma
     fig = px.histogram(car_data, x="odometer")
 
     # mostrar un gráfico Plotly interactivo
